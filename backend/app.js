3888059484a0b16942047f8e9842625c6398862d
VIvek Halder <vivekh.it.ug@jadavpuruniversity.in>
2024-02-25 22:54:40 +0530

    add origin

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import { limit } from './constants.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN, "https://astro-quantum-ju.onrender.com"],
     credentials: true,
 }));
 app.use(express.json({limit: `${limit}`}));
