180448b267ccb469cadbe653787077756716f4c7
VIvek Halder <vivekh.it.ug@jadavpuruniversity.in>
2024-03-09 12:42:42 +0530

    check

--- backend/middlewares/errorHandler.middleware.js
+++ backend/middlewares/errorHandler.middleware.js
@@ -1,6 +1,8 @@
 export const errorHandler = (err, req, res, next) => {
     console.log(err.stack);
 
+    console.log("Reached.");
+
     const statusCode = err.statusCode || 500;
     const message = err.message || "Something went wrong.";
 
