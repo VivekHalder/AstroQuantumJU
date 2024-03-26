858c21dcf0895ff32dba1e44ef92dd0e5180165c
VIvek Halder <vivekh.it.ug@jadavpuruniversity.in>
2024-03-26 17:53:28 +0530

    notification schema added

---
+++ backend/models/notification.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+}, {timestamps: true});
+
+export const Notification = mongoose.model("Notification", notificationSchema);
