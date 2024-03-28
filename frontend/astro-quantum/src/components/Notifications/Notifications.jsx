24258382b51ee561ab6c6bc5e2651486b7758a9e
VIvek Halder <vivekh.it.ug@jadavpuruniversity.in>
2024-03-28 15:55:53 +0530

    Notifications divided into seen and unseen

--- frontend/astro-quantum/src/components/Notifications/Notifications.jsx
+++ frontend/astro-quantum/src/components/Notifications/Notifications.jsx
@@ -18,21 +18,76 @@ function Notifications() {
     }
   };
 
+  const unseenNotification = notifications.filter(notification => !notification.read);
+  const seenNotification = notifications.filter(notification => notification.read);
+
   return (
     <div className="bg-black h-full text-white">
       <h1 className="text-5xl font-bold text-center mb-8">Notifications</h1>
       <div className="container mx-auto">
-        {notifications && notifications.length === 0 ? (
+        {!notifications || notifications.length === 0 ? (
           <p className="text-center text-xl">No notifications found.</p>
         ) : (
-          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            {notifications && notifications.map((notification, index) => (
-              <div key={notification._id} className="p-4 border border-white rounded-md">
-                <h2 className="text-lg font-semibold mb-2">{notification.message}</h2>
-                <p className="text-sm text-gray-400">{notification.createdAt}</p>
-              </div>
-            ))}
-          </div>
+          <>
+            <div>
+                {
+                    unseenNotification && unseenNotification.length > 0 &&
+                    <>
+                        <div className='text-3xl font-semibold mb-4'>
+                            Unseen Notifications
+                        </div>
+                        <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+                            {
+                                unseenNotification.map(notification => (
+                                    <div key={notification._id} className="p-4 border border-white rounded-md">
+                                        <h2 className='text-lg font-semibold mb-2'>
+                                            {
+                                                notification.message
+                                            }
+                                        </h2>
+                                        <p className='text-sm text-gray-400'>
+                                            {
+                                                notification.createdAt
+                                            }
+                                        </p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>
+                }
+            </div>
+            <div>
+                {
+                    seenNotification && seenNotification.length > 0 &&
+                    (
+                        <>
+                            <div className='text-3xl font-semibold mb-4'>
+                                Seen Notifications
+                            </div>
+                            <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+                                {
+                                    seenNotification.map(notification => (
+                                        <div key={notification._id} className="p-4 border border-white rounded-md">
+                                            <h2 className='text-lg font-semibold mb-2'>
+                                                {
+                                                    notification.message
+                                                }
+                                            </h2>
+                                            <p className='text-sm text-gray-400'>
+                                                {
+                                                    notification.createdAt
+                                                }
+                                            </p>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    )
+                }
+            </div>
+          </>
         )}
       </div>
     </div>
